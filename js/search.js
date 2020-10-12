586221d0f9508b520a57792e3abc15dce52b1084
Paulina Atienza <PaulinaAtienza@users.noreply.github.com>
2020-10-12 13:28:02 -0500

    Adjust js

---
+++ js/search.js
@@ -0,0 +1,13 @@
+const searchBar = document.getElementById("search");
+let gallery = document.querySelectorAll(".gallery a");
+searchBar.addEventListener("keyup", (e) => {
+  const searchTerm = e.target.value.toLowerCase();
+  for (let i = 0; i < gallery.length; i++) {
+    const searchInput = gallery[i].getAttribute("data-caption");
+  if (searchInput.toLowerCase().includes(searchTerm)) {
+    gallery[i].style.display = "";
+  } else {
+    gallery[i].style.display = "none";
+  }
+}
+});
