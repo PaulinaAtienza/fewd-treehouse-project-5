7b0e5df0b68955228a93dc66054f3f333c4c8d5d
Paulina Atienza <PaulinaAtienza@users.noreply.github.com>
2020-10-12 13:25:37 -0500

    Adjust css, html, and js files

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 baguetteBox.run('.gallery');
+
+const searchBar = document.getElementById("search");
+let gallery = document.querySelectorAll(".gallery a");
+searchBar.addEventListener("keyup", (e) => {
+  const searchTerm = e.target.value.toLowerCase();
+  for (let i = 0; i < gallery.length; i++) {
+    const searchInput = gallery[i].getAttribute("data-caption");
+  if (searchInput.toLowerCase().includes(searchTerm)) {
+    gallery[i].style.display = "";
+  } else {
+    gallery[i].style.display = "none";
+  }
+}
+});
